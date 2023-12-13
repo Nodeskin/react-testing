85872226e17984b4fcec28d9fedcc02b6eca2ae4
nodeSkin <shittujamiu@gmail.com>
2023-12-13 12:27:07 +0100

    added app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,27 @@
-// import { render, screen } from "@testing-library/react";
-// import App from "./App";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-// test("renders learn react link", () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
-// test("renders 3 list items", () => {
-//   render(<App />);
-//   const listItems = screen.getAllByRole(/listItem/i);
-//   expect(listItems.length).toEqual(3);
-// });
+test("renders 3 list items", () => {
+  render(<App />);
+  const listItems = screen.getAllByRole(/listItem/i);
+  expect(listItems.length).toEqual(3);
+});
  
-// test("renders title", () => {
-//   render(<App />);
-//   const title = screen.getByTestId("mytestid");
-//   expect(title).toBeInTheDocument();
-// });
+test("renders title", () => {
+  render(<App />);
+  const title = screen.getByTestId("mytestid");
+  expect(title).toBeInTheDocument();
+});
 
-// test("Sum should be 6", () => {
-//   render(<App />);
-//   const sum = screen.getByTitle("sum");
-//   expect(sum.textContent).toBe("6");
-// });
+test("Sum should be 6", () => {
+  render(<App />);
+  const sum = screen.getByTitle("sum");
+  expect(sum.textContent).toBe("6");
+});
  
